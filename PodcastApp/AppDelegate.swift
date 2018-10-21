4bc784b3e156a75e98c51b8ac2b56f48cbf3f369
AndrewJenson <awjenson@gmail.com>
2018-10-21 12:51:55 -0700

    Setup MainTabBarController

--- PodcastApp/AppDelegate.swift
+++ PodcastApp/AppDelegate.swift
@@ -14,8 +14,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        // override Storyboard at launch time
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        window?.rootViewController = MainTabBarController()
+
         return true
     }
 
