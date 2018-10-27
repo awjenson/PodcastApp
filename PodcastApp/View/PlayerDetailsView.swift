50ef5b8de0fe3477f25fcc3db1ac706384f82fe9
AndrewJenson <awjenson@gmail.com>
2018-10-26 20:46:09 -0700

    Play Episode with AVPlayer

--- PodcastApp/View/PlayerDetailsView.swift
+++ PodcastApp/View/PlayerDetailsView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVKit // play audio files
 
 class PlayerDetailsView: UIView {
 
@@ -15,13 +16,34 @@ class PlayerDetailsView: UIView {
             episodeTitleLabel.text = episode.title
             authorLabel.text = episode.author
 
+            playEpisode()
+
             guard let url = URL(string: episode.imageUrl ?? "") else {return}
 
             episodeImageView.sd_setImage(with: url) // removed completionHandler because default is nil
+
+
         }
     }
 
+    fileprivate func playEpisode() {
+        // pList > App Transport Security > Allow Arbitrary Loads > YES, to allow https
+        print("Trying to play episode at url", episode.streamUrl)
 
+        guard let url = URL(string: episode.streamUrl) else {return}
+
+        let playerItem = AVPlayerItem(url: url)
+        player.replaceCurrentItem(with: playerItem)
+        player.play()
+    }
+
+    let player: AVPlayer = {
+        let avPlayer = AVPlayer()
+        avPlayer.automaticallyWaitsToMinimizeStalling = false
+        return avPlayer
+    }()
+
+    // MARK: - IBOutlet
 
     @IBOutlet weak var episodeImageView: UIImageView!
 
@@ -33,8 +55,26 @@ class PlayerDetailsView: UIView {
 
     @IBOutlet weak var authorLabel: UILabel!
 
-    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var playPauseButton: UIButton! {
+        didSet {
+            playPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+            playPauseButton.addTarget(self, action: #selector(handlePlayPause), for: .touchUpInside)
+        }
+    }
 
+    @objc func handlePlayPause() {
+
+        if player.timeControlStatus == .paused {
+            player.play()
+            playPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+        } else {
+            player.pause()
+            playPauseButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
+        }
+    }
+
+
+    // MARK: - IBAction
 
     @IBAction func handleDismiss(_ sender: Any) {
         self.removeFromSuperview() // superView is the window defined in EpisodesController
