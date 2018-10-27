953ab16f97c1347ae29723453193ea6a5af6b282
AndrewJenson <awjenson@gmail.com>
2018-10-26 19:29:59 -0700

    Player Details View Design UI Continued

--- PodcastApp/View/PlayerDetailsView.swift
+++ PodcastApp/View/PlayerDetailsView.swift
@@ -13,6 +13,7 @@ class PlayerDetailsView: UIView {
     var episode: Episode! {
         didSet {
             episodeTitleLabel.text = episode.title
+            authorLabel.text = episode.author
 
             guard let url = URL(string: episode.imageUrl ?? "") else {return}
 
@@ -20,10 +21,20 @@ class PlayerDetailsView: UIView {
         }
     }
 
+
+
     @IBOutlet weak var episodeImageView: UIImageView!
 
-    @IBOutlet weak var episodeTitleLabel: UILabel!
-    
+    @IBOutlet weak var episodeTitleLabel: UILabel! {
+        didSet {
+            episodeTitleLabel.numberOfLines = 2
+        }
+    }
+
+    @IBOutlet weak var authorLabel: UILabel!
+
+    @IBOutlet weak var playPauseButton: UIButton!
+
 
     @IBAction func handleDismiss(_ sender: Any) {
         self.removeFromSuperview() // superView is the window defined in EpisodesController
